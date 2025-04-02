f16206f403874220615654a919aed2c6228dc89c
Abdulmalik Abiodun Busari <abusari@eceprog1.ecn.purdue.edu>
2025-04-02 01:56:36 -0400

    rmgrkg

--- queue.c
+++ queue.c
@@ -1,12 +1,6 @@
 #include "queue.h"
 #include "tile_game.h"
 
-
-
-// Function prototypes (ensure they're implemented somewhere)
-// bool is_solved(struct game_state state);
-// int generate_next_states(struct game_state current, struct game_state next_states[4]);
-
 // Enqueue a game state into the queue
 void enqueue(struct queue *q, struct game_state state) 
 {
@@ -25,6 +19,57 @@ struct game_state dequeue(struct queue *q)
     return deserialize(serialized_state);
 }
 
+// Check if a game state is solved
+bool is_solved(struct game_state state) 
+{
+    struct game_state solved_state = {
+        .tiles = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 0}
+        },
+        .empty_row = 3,
+        .empty_col = 3,
+        .num_steps = 0
+    };
+
+    return serialize(state) == serialize(solved_state);
+}
+
+// Generate next possible states
+int generate_next_states(struct game_state current, struct game_state next_states[4]) 
+{
+    int num_next = 0;
+
+    // Try moving the empty tile in all four directions
+    if (current.empty_row > 0) { // Can move up
+        struct game_state next = current;
+        move_up(&next);
+        next_states[num_next++] = next;
+    }
+
+    if (current.empty_row < 3) { // Can move down
+        struct game_state next = current;
+        move_down(&next);
+        next_states[num_next++] = next;
+    }
+
+    if (current.empty_col > 0) { // Can move left
+        struct game_state next = current;
+        move_left(&next);
+        next_states[num_next++] = next;
+    }
+
+    if (current.empty_col < 3) { // Can move right
+        struct game_state next = current;
+        move_right(&next);
+        next_states[num_next++] = next;
+    }
+
+    return num_next;
+}
+
 // Perform BFS to find the shortest number of moves
 int number_of_moves(struct game_state start) 
 {
@@ -40,7 +85,7 @@ int number_of_moves(struct game_state start)
         if (is_solved(current)) 
         {
             free_list(q.data);
-            return current.num_steps; // Use num_steps instead of num_moves
+            return current.num_steps; // Return the number of steps (moves) taken
         }
 
         struct game_state next_states[4];
